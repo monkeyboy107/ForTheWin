a202a5d0a75c5cf44ee3bc08178363308a226958
Josiah Kerley <josiahkerley@gmail.com>
2014-09-28 14:43:07 -0600

    Started the Characters.py class file

---
+++ codebase/Characters.py
@@ -0,0 +1,24 @@
+#!/usr/bin/py
+##
+##    This file contains the class(es) pertaining
+##  to characters.
+##
+##    Classes: 
+##  Characters
+
+
+class Characters:
+
+	## Attributes
+	name = None
+	health = 100
+	items = {}
+
+	##-Methods-##
+
+	## This method executes when the class is loaded
+	def __init__(self)
+		pass
+
+	def say(self,words):
+		test = "\t[%s]: %s"%(self.name,words)
